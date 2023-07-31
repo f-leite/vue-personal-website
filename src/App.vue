8ea3a7925dde1014165e2693bf926d4aef04c365
f-leite <fleite1202@gmail.com>
2023-07-31 18:19:56 +0100

    test commit

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Small little change!" />
+      <HelloWorld msg="Small little change again!" />
     </div>
   </header>
 
