c39ad9285dd1e5070e12f27dcc688905847bd4e7
f-leite <fleite1202@gmail.com>
2023-08-06 16:10:54 +0100

    Fixed images - original

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-image" style="background-image: url('src/assets/images/city-night-bg.jpg'); height: 100vh;">
+  <div class="bg-image" style="background-image: url('/images/city-night-bg.jpg'); height: 100vh;">
     <div class="mask" style="background-color: hsla(0, 0%, 0%, 0.5)">
       <Navbar />
       <router-view/>
